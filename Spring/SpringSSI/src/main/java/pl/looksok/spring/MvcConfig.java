79141ccff121fd455526e4bbdf1c5fd084750961
yacekmm <fake@gmail.com>
2014-06-15 14:22:37 +0200

    initial commit

---
+++ Spring/SpringSSI/src/main/java/pl/looksok/spring/MvcConfig.java
@@ -0,0 +1,13 @@
+package pl.looksok.spring;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+public class MvcConfig extends WebMvcAutoConfigurationAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/greeting").setViewName("greeting");
+	}
+}
