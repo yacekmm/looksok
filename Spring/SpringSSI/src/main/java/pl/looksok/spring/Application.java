d1e2b460c17c724b95a845424be3c7cd1b4351ae
yacekmm <fake@gmail.com>
2014-06-15 16:45:54 +0200

    complete working example

--- Spring/SpringSSI/src/main/java/pl/looksok/spring/Application.java
+++ Spring/SpringSSI/src/main/java/pl/looksok/spring/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @Configuration
-@ComponentScan("pl.looksok.spring")
+@ComponentScan({"pl.looksok.spring"})
 public class Application {
 
 	public static void main(String[] args) {
