7f6cabdea603c9e51a40cd89c2944fbcef97ae78
yacekmm <fake@gmail.com>
2014-07-01 19:33:00 +0200

    messages in thymeleaf

--- Spring/SpringStringExternalization/src/main/java/pl/looksok/spring/config/MvcConfig.java
+++ Spring/SpringStringExternalization/src/main/java/pl/looksok/spring/config/MvcConfig.java
@@ -1,12 +1,23 @@
 package pl.looksok.spring.config;
 
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 public class MvcConfig extends WebMvcAutoConfigurationAdapter {
 
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("locale/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("hello");
