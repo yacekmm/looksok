71695f22d0adcb53e11559f64633f29a9793c392
Jacek Milewski <jacek.milewski.k@gmail.com>
2014-10-19 10:12:16 +0200

    Spring Security initial commit + some strange changes in another project

--- Spring/SpringSecurity/src/main/java/pl/looksok/spring/Application.java
+++ Spring/SpringSecurity/src/main/java/pl/looksok/spring/Application.java
@@ -1,14 +1,9 @@
 package pl.looksok.spring;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @EnableAutoConfiguration
 @Configuration
