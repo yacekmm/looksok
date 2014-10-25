63649ee659009a9b930d523a354fd6d7691ee2bd
Jacek Milewski <jacek.milewski.k@gmail.com>
2014-10-25 12:35:20 +0200

    basic web security config

---
+++ Spring/SpringSecurity/src/main/java/pl/looksok/spring/security/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package pl.looksok.spring.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("pwd").roles("USER");
+	}
+}
