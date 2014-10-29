fc00133b9db60837b3d1ca62eaa930eaadb7fea8
Jacek Milewski <jacek.milewski.k@gmail.com>
2014-10-29 21:27:43 +0100

    tutroial fix - commented lines for custom login page

--- Spring/SpringSecurity/src/main/java/pl/looksok/spring/security/config/WebSecurityConfig.java
+++ Spring/SpringSecurity/src/main/java/pl/looksok/spring/security/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package pl.looksok.spring.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -11,7 +10,8 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	protected void configure(HttpSecurity http) throws Exception{
+//	UNCOMMENT if you want to provide your custom login page
+/*	protected void configure(HttpSecurity http) throws Exception{
 		
 		http
 			.authorizeRequests()
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/login")
 				.permitAll();
 	}
+*/
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
