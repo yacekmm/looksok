570100b55ad95a5cabec90a3cca6a5be726497af
Jacek Milewski <jacek.milewski.k@gmail.com>
2014-10-29 21:25:16 +0100

    tutorial initial commit

--- Spring/SpringSecurity/src/main/java/pl/looksok/spring/security/config/WebSecurityConfig.java
+++ Spring/SpringSecurity/src/main/java/pl/looksok/spring/security/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package pl.looksok.spring.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -11,21 +10,8 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	protected void configure(HttpSecurity http) throws Exception{
-		
-		http
-			.authorizeRequests()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll();
-	}
-	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password("pwd").roles("USER");
+		auth.authenticationProvider(new MyAuthenticationProvider());
 	}
 }
