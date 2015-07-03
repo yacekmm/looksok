06182988d6cf54bf8219806984ae37071cf4e9b5
Jacek Milewski <jacek.milewski.k@gmail.com>
2015-07-03 21:23:58 +0200

    project initial config

---
+++ Guava/GuavaCacheDemo/src/main/java/com/looksok/cache/Main.java
@@ -0,0 +1,11 @@
+package com.looksok.cache;
+
+/**
+ * Created by Jacek Milewski (looksok.wordpress.com)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
