46127727729070eb7f1a2058fda8e19eb9bbeca7
Jacek Milewski <jacek.milewski.k@gmail.com>
2015-09-05 16:52:10 +0200

    LongRunningProcess implementation works

--- Guava/RateLimiter/src/main/java/com/looksok/ratelimiter/LongRunningProcess.java
+++ Guava/RateLimiter/src/main/java/com/looksok/ratelimiter/LongRunningProcess.java
@@ -1,5 +1,7 @@
 package com.looksok.ratelimiter;
 
+import java.math.BigDecimal;
+
 public class LongRunningProcess implements Runnable{
 
     private int stepsCount;
@@ -13,7 +15,13 @@ public class LongRunningProcess implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < stepsCount; i++) {
-            guiUpdater.updateGui(progerss is);
+            guiUpdater.updateGui("progress is " + round((float)i/stepsCount * 100, 2) + "%");
         }
+    }
+
+    private BigDecimal round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd;
     }
 }
--- Guava/RateLimiter/src/main/java/com/looksok/ratelimiter/Main.java
+++ Guava/RateLimiter/src/main/java/com/looksok/ratelimiter/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String args[]){
         System.out.println("Welcome my precious!");
+
+        LongRunningProcess process = new LongRunningProcess(100, new ProgressGuiUpdater());
+        new Thread(process).run();
     }
 }
