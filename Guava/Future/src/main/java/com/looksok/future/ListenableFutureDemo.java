b4474fab007293502db881a832e69355dca29f8b
yacekmm <jacek.milewski.k@gmail.com>
2015-09-13 21:06:33 +0200

    ListenableFuture Demo

--- Guava/Future/src/main/java/com/looksok/future/ListenableFutureDemo.java
+++ Guava/Future/src/main/java/com/looksok/future/ListenableFutureDemo.java
@@ -1,10 +1,11 @@
 package com.looksok.future;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class ListenableFutureDemo {
     public String executeLongRunningTask() throws InterruptedException {
         Thread.sleep(5000);
-        return "Evaluation finished at " + LocalDate.now();
+        return "Evaluation finished at " + LocalTime.now();
     }
 }
--- Guava/Future/src/main/java/com/looksok/future/Main.java
+++ Guava/Future/src/main/java/com/looksok/future/Main.java
@@ -1,6 +1,11 @@
 package com.looksok.future;
 
+import com.google.common.util.concurrent.*;
+
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class Main {
@@ -8,10 +13,34 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println(LocalTime.now() + " Hello future!");
 
-        ListenableFutureDemo demo = new ListenableFutureDemo();
+        final ListenableFutureDemo demo = new ListenableFutureDemo();
 
-        String result = demo.executeLongRunningTask();
+        //create executor service to submit all future tasks
+        ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
 
-        System.out.println(LocalTime.now() + " Main method completed: " + result);
+        //execute non-blocking task
+        ListenableFuture<String> listenableFuture = service.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return demo.executeLongRunningTask();
+            }
+        });
+
+        //add success and failure handler to listenableFuture object
+        Futures.addCallback(listenableFuture, new FutureCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                System.out.println(LocalTime.now() + " Task completed successfuly with result: " + result);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println(LocalTime.now() + " Task failed with result: " + t.getMessage());
+            }
+        });
+
+//        String result = demo.executeLongRunningTask();
+
+        System.out.println(LocalTime.now() + " Main method completed");
     }
 }
