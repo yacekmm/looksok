12a9ba24adfb736d1e7d8435a21f669a688b53e5
yacekmm <jacek.milewski.k@gmail.com>
2015-09-13 20:49:54 +0200

    blocking long running task

--- Guava/Future/src/main/java/com/looksok/future/Main.java
+++ Guava/Future/src/main/java/com/looksok/future/Main.java
@@ -1,8 +1,17 @@
 package com.looksok.future;
 
+import java.time.LocalTime;
+import java.util.logging.Logger;
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("Hello future!");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(LocalTime.now() + " Hello future!");
+
+        ListenableFutureDemo demo = new ListenableFutureDemo();
+
+        demo.executeLongRunningTask();
+
+        System.out.println(LocalTime.now() + " Main method completed!");
     }
 }
