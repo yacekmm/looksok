48a7197b5c34ed9c657f97095335849dec1e73cd
Jacek Milewski <jacek.milewski.k@gmail.com>
2015-10-17 13:33:01 +0200

    Joiner on Map demo

--- Guava/GettingStarted/src/main/java/com/looksok/guava/basic/JoinerClassDemo.java
+++ Guava/GettingStarted/src/main/java/com/looksok/guava/basic/JoinerClassDemo.java
@@ -2,13 +2,16 @@ package com.looksok.guava.basic;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import java.util.List;
+import java.util.Map;
 
 public class JoinerClassDemo {
 
-    private List<String> listOfStrings = Lists.newArrayList("egg", "apple", null, "pineapple", "orange");
     private static final String SEPARATOR = ", ";
+
+    private List<String> listOfStrings = Lists.newArrayList("egg", "apple", null, "pineapple", "orange");
 
     public void runDemo() {
         System.out.println("\n=== Running Joiner class demo: Joining list of Strings ===");
@@ -17,7 +20,10 @@ public class JoinerClassDemo {
         runJoinInJavaWayWithLambdas();
         runJoinWithGuavaJoiner();
         runJoinWithGuavaJoinerNullSubstitution();
+        runJoinWithGuavaJoinerPlusExtraElements();
+        runGuavaJoinerOnMap();
     }
+
 
     private void runJoinWithGuavaJoiner() {
 
@@ -26,11 +32,38 @@ public class JoinerClassDemo {
         System.out.println("\nGUAVA JOINER result:\n\t" + result);
     }
 
+
+    private void runJoinWithGuavaJoinerPlusExtraElements() {
+
+        String result = Joiner.on(SEPARATOR).skipNulls().join(listOfStrings);
+
+        StringBuilder builder = new StringBuilder(result);
+        String extendedResult = Joiner.on(SEPARATOR).skipNulls().join(builder, "Strawberry");
+
+        System.out.println("\nGUAVA JOINER result with extension:\n\t" + extendedResult);
+    }
+
     private void runJoinWithGuavaJoinerNullSubstitution() {
 
         String result = Joiner.on(SEPARATOR).useForNull("EMPTY VALUE").join(listOfStrings);
 
         System.out.println("\nGUAVA JOINER with null substitution result:\n\t" + result);
+    }
+
+    private void runGuavaJoinerOnMap() {
+        Map<String, Integer> map = buildStringIntegerMap();
+
+        String mapJoinResult = Joiner.on(SEPARATOR + "\n\t").withKeyValueSeparator(" has length of: ").join(map);
+
+        System.out.println("\nGUAVA JOINER on MAP result:\n\t" + mapJoinResult);
+    }
+
+    private Map<String, Integer> buildStringIntegerMap() {
+        Map<String, Integer> map = Maps.newHashMap();
+        listOfStrings.stream()
+                .filter(item -> item != null)
+                .forEach(item -> map.put(item, item.length()));
+        return map;
     }
 
     private void runJoinInJavaWay() {
