6184088474049bf538ef1e6e66eaae25d25f76e9
Jacek Milewski <jacek.milewski@orange.com>
2015-11-07 13:27:07 +0100

    create, properties and symlink

---
+++ Java/Java7NIO.2/src/main/java/com/looksok/Main.java
@@ -0,0 +1,10 @@
+package com.looksok;
+
+import com.looksok.path.PathDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new PathDemo().run();
+    }
+}
---
+++ Java/Java7NIO.2/src/main/java/com/looksok/path/PathDemo.java
@@ -0,0 +1,32 @@
+package com.looksok.path;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathDemo {
+
+    public void run() {
+        //get current absolute path on default filesystem
+        Path path = Paths.get("").toAbsolutePath();
+        System.out.println("Current working dir absolute path is: " + path);
+
+        //analyze linux path properties
+        path = Paths.get("/var/lib/jenkins/jobs/sampleJob");
+        System.out.println("\nSample linux path that will be analyzed: " + path.toAbsolutePath());
+        System.out.println("Number of segments in path: " + path.getNameCount());
+        System.out.println("Parent in path: " + path.getParent());
+        System.out.println("Root on this filesystem: " + path.getRoot());
+        System.out.println("Subpath (2 elements up): " + path.subpath(path.getNameCount() - 2, path.getNameCount()));
+
+        //resolve symlink target file location
+        try {
+            path = Paths.get("/var/log/logFile");
+            path.toRealPath();
+        } catch (IOException e) {
+            System.out.println("File odes not exist: " + path);
+        }
+    }
+}
