ad7f810b119fbb280900e3b0bfd5c1216f729661
Jacek Milewski <jacek.milewski@orange.com>
2016-06-19 13:32:17 +0200

    annotated JMX attrs and operations

--- Spring/SpringJMX/src/main/java/pl/looksok/spring/controller/MyController.java
+++ Spring/SpringJMX/src/main/java/pl/looksok/spring/controller/MyController.java
@@ -2,18 +2,41 @@ package pl.looksok.spring.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
+@ManagedResource(objectName = "jmxDemo:name=MyController")
 public class MyController {
 
     private int myJmxValue = 25;
 
-    @RequestMapping(value = "/api/my-jmx-value")
+    private Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @ManagedAttribute
+    public void setMyJmxValue(int myJmxValue) {
+        this.myJmxValue = myJmxValue;
+    }
+
+    @ManagedAttribute
     public int getMyJmxValue(){
         return myJmxValue;
+    }
+
+    @RequestMapping(value = "/api/my-jmx-value", method = GET)
+    public int getManagedAttributeValue(){
+        return myJmxValue;
+    }
+
+    @ManagedOperation
+    public void logMyJmxValue(String requester){
+        log.info("{} requested to log JmxManagedValue which is: {}", requester, myJmxValue);
     }
 }
