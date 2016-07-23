afb20eda00779773459da1fb29918cb7435c8a9d
Jacek Milewski <jacek.milewski.k@gmail.com>
2016-07-23 19:00:32 +0200

    BasicAuth initial commit

---
+++ Spring/SecurityBasicAuth/src/main/java/com/looksok/ApisecurityApplication.java
@@ -0,0 +1,12 @@
+package com.looksok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisecurityApplication.class, args);
+	}
+}
---
+++ Spring/SecurityBasicAuth/src/main/java/com/looksok/controller/SecuredController.java
@@ -0,0 +1,22 @@
+package com.looksok.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping("/api")
+public class SecuredController {
+
+    @RequestMapping(value = "/secured", method = GET, produces = TEXT_PLAIN_VALUE)
+    public String securedGet(){
+        return "You have access to secured API!";
+    }
+
+    @RequestMapping(value = "/open", method = GET, produces = TEXT_PLAIN_VALUE)
+    public String openedGet(){
+        return "this API does not require authentication";
+    }
+}
